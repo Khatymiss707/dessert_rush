db1799a8cfb55128614e5b6d176516a05e03071c
keven malric <112128314+KevenMalric@users.noreply.github.com>
2024-12-03 11:27:39 -0500

    sdgadhj

---
+++ tp03_dessert_rush/Assets/scripts/control.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour
+{
+    public GameObject[] client;
+    public GameObject client_actuel;
+
+    private void Start()
+    {
+        StartCoroutine("first_client");
+    }
+
+    public IEnumerator first_client() 
+    {
+        yield return new WaitForSeconds(8);
+        int xcount = Random.Range(1, 5);
+        client_actuel = client[xcount];
+        client[xcount].GetComponent<Animator>().Play("nom_anim");
+        yield break;
+    }
+
+    public void reussite()
+    {
+       
+    } 
+}
---
+++ tp03_dessert_rush/Assets/validation_commande.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class validation_commande : MonoBehaviour
+{
+    public script control
+    public void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
