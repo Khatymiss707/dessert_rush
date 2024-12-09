d7c12a1368e2c74955e5c8fc732cb202e3d2e2cb
keven malric <112128314+KevenMalric@users.noreply.github.com>
2024-12-09 16:03:05 -0500

    yukeuley;

--- tp03_dessert_rush/Assets/scripts/validation_commande.cs
+++ tp03_dessert_rush/Assets/scripts/validation_commande.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class validation_commande : MonoBehaviour
 {
     public control script;
-
-
+    public int count_yes;
+    public int count_no;
+    public int count;
+    public TextMeshProUGUI total;
+    public TextMeshProUGUI succes;
+    public TextMeshProUGUI fail;
+    public GameObject fin;
     public IEnumerator mochi_despawn(GameObject mochi)
     {
         yield return new WaitForSeconds(3);
@@ -16,15 +22,27 @@ public class validation_commande : MonoBehaviour
 
     public void OnTriggerEnter(Collider other)
     {
+        count++;
+        total.text = count.ToString();
+        if(count < 10) { 
         if(other.tag == script.client_actuel.tag)
         {
             script.reussite();
             StartCoroutine("mochi_despawn", other.gameObject);
+            count_yes++;
+            succes.text = count_yes.ToString();
+
         }
         else { 
             script.failure();
             StartCoroutine("mochi_despawn", other.gameObject);
+            count_no++;
+            fail.text = count_no.ToString();
 
         }
-    }
+    }else if(count == 10)
+        {
+            fin.SetActive(true);
+        }
+   }
 }
